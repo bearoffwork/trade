6e1df883992fc0a39eb6f29904b18c7d7332bfdb
Bear.Y <codes@rra3b.io>
2024-12-10 17:01:36 +0800

    allow empty item_users

--- app/Services/MoneyService.php
+++ app/Services/MoneyService.php
@@ -17,28 +17,33 @@ readonly class MoneyService
         info('doShare #'.$item->id, ['iid' => $item->id, 'total' => $item->posted_amt, 'fund_rate' => $item->fund_rate]);
         $divideBy = $item->Users->count();
 
-        // 公積金抽成無條件進位
-        $fundShare = bcceil(bcmul($item->posted_amt, $item->fund_rate, 2));
-        info('doShare #'.$item->id, ['iid' => $item->id, 'fund' => $fundShare]);
+        if ($divideBy === 0) {
+            $fundShare = $item->posted_amt;
+        } else {
+            // 公積金抽成無條件進位
+            $fundShare = bcceil(bcmul($item->posted_amt, $item->fund_rate, 2));
+            info('doShare #'.$item->id, ['iid' => $item->id, 'fund' => $fundShare]);
 
-        // total - 公積金抽成
-        $totalShare = bcsub($item->posted_amt, $fundShare, 0);
-        info('doShare #'.$item->id, ['iid' => $item->id, 'totalShare' => $totalShare]);
+            // total - 公積金抽成
+            $totalShare = bcsub($item->posted_amt, $fundShare, 0);
+            info('doShare #'.$item->id, ['iid' => $item->id, 'totalShare' => $totalShare]);
 
-        // 每人分成取整數
-        $eachShare = bcfloor(bcdiv($totalShare, $divideBy, 2));
-        info('doShare #'.$item->id, ['iid' => $item->id, 'each' => $eachShare, 'divideBy' => $divideBy]);
+            // 每人分成取整數
+            $eachShare = bcfloor(bcdiv($totalShare, $divideBy, 2));
+            info('doShare #'.$item->id, ['iid' => $item->id, 'each' => $eachShare, 'divideBy' => $divideBy]);
 
-        // 分成餘數
-        $modulus = bcsub($totalShare, bcmul($eachShare, $divideBy));
-        info('doShare #'.$item->id, ['iid' => $item->id, 'mod' => $modulus]);
+            // 分成餘數
+            $modulus = bcsub($totalShare, bcmul($eachShare, $divideBy));
+            info('doShare #'.$item->id, ['iid' => $item->id, 'mod' => $modulus]);
 
-        // 餘數歸公
-        $fundShare = bcadd($fundShare, $modulus, 0);
-        info('doShare #'.$item->id, ['iid' => $item->id, 'fund+mod' => $fundShare]);
+            // 餘數歸公
+            $fundShare = bcadd($fundShare, $modulus, 0);
+            info('doShare #'.$item->id, ['iid' => $item->id, 'fund+mod' => $fundShare]);
 
-        if (bccomp($item->posted_amt, bcadd($fundShare, bcmul($eachShare, $divideBy))) !== 0) {
-            throw new LogicException('Calculation error');
+            if (bccomp($item->posted_amt, bcadd($fundShare, bcmul($eachShare, $divideBy))) !== 0) {
+                throw new LogicException('Calculation error');
+            }
+
         }
 
         /** @var FundRecord $fundRecord */
@@ -47,6 +52,9 @@ readonly class MoneyService
         $fundRecord->amount = $fundShare;
         $fundRecord->balance = bcadd($fundRecord->balance, $fundShare);
         $fundRecord->save();
+        if ($divideBy === 0) {
+            return;
+        }
 
         $item->Users->each(static function (User $user) use ($eachShare, $item, $fundRecord) {
             /** @var WalletRecord $walletRecord */
